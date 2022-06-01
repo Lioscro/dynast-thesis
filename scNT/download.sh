1d8ef5b7eea9b2143cbfda3727dacec8d27aa260
Lioscro <phoenixter96@gmail.com>
2022-06-01 16:24:12 -0400

    initial commit

---
+++ scNT/download.sh
@@ -0,0 +1,4 @@
+# Download the scNT-seq Cortical-KCl data from the SRA using fasterq-dump
+# The resulting FASTQs will be saved in a subdirectory with the SRR as the name in the data/ directory.
+while read accession; do
+
